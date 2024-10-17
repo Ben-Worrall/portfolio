054eeb526e71ef79e441c6d08eef72a0d4c3155f
Ben-Worrall <benworrallpc@gmail.com>
2024-10-17 17:53:58 +0200

    a bit more

--- src/app/fileExplorer/functions/fileExplorer-RightPanel-Portfolio.js
+++ src/app/fileExplorer/functions/fileExplorer-RightPanel-Portfolio.js
@@ -3,15 +3,22 @@ const fileExplorer_RightPanel_Portfolio = () => {
 
 
     window.addEventListener('click',(e)=>{
-        if(e.target.id !== "RightPanel-Certifications-Subfolders-CyberSec-holder" && e.target.id !== "RightPanel-Certifications-Subfolders-CyberSec-icon" && e.target.id !== "RightPanel-Certifications-Subfolders-CyberSec-text" ){
-            let holder = document.getElementById('RightPanel-Certifications-Subfolders-CyberSec-holder')
+        if(e.target.id !== "RightPanel-Portfolio-Subfolders-Certifications-holder" && e.target.id !== "RightPanel-Portfolio-Subfolders-Certifications-icon" && e.target.id !== "RightPanel-Portfolio-Subfolders-Certifications-text" && e.target.id !== "RightPanel-Portfolio-Subfolders-Certifications-type" ){
+            let holder = document.getElementById('RightPanel-Portfolio-Subfolders-Certifications-holder')
             holder.style.backgroundColor = 'white'
             holder.style.color = 'black'
             holder.style.outline = ''
             
         }
-        if(e.target.id !== "RightPanel-Certifications-Subfolders-Dev-holder" && e.target.id !== "RightPanel-Certifications-Subfolders-Dev-icon" && e.target.id !== "RightPanel-Certifications-Subfolders-Dev-text" ){
-            let holder = document.getElementById('RightPanel-Certifications-Subfolders-Dev-holder')
+        if(e.target.id !== "RightPanel-Portfolio-Subfolders-Projects-holder" && e.target.id !== "RightPanel-Portfolio-Subfolders-Projects-icon" && e.target.id !== "RightPanel-Portfolio-Subfolders-Projects-text" && e.target.id !== "RightPanel-Portfolio-Subfolders-Projects-type"){
+            let holder = document.getElementById('RightPanel-Portfolio-Subfolders-Projects-holder')
+            holder.style.backgroundColor = 'white'
+            holder.style.color = 'black'
+            holder.style.outline = ''
+            
+        }
+        if(e.target.id !== "RightPanel-Portfolio-Subfolders-AboutMe-holder" && e.target.id !== "RightPanel-Portfolio-Subfolders-AboutMe-icon" && e.target.id !== "RightPanel-Portfolio-Subfolders-AboutMe-text" && e.target.id !== "RightPanel-Portfolio-Subfolders-AboutMe-type"){
+            let holder = document.getElementById('RightPanel-Portfolio-Subfolders-AboutMe-holder')
             holder.style.backgroundColor = 'white'
             holder.style.color = 'black'
             holder.style.outline = ''
@@ -22,21 +29,21 @@ const fileExplorer_RightPanel_Portfolio = () => {
 
 
     function go_blue_on_clickCyber(){
-        let holder = document.getElementById('RightPanel-Certifications-Subfolders-CyberSec-holder')
+        let holder = document.getElementById('RightPanel-Portfolio-Subfolders-Certifications-holder')
         holder.style.backgroundColor = 'blue'
         holder.style.color = 'white'
         holder.style.outline = '0.2vw solid black '
         holder.style.outlineStyle = 'dotted'
     }
     function go_blue_on_clickDev(){
-        let holder = document.getElementById('RightPanel-Certifications-Subfolders-Dev-holder')
+        let holder = document.getElementById('RightPanel-Portfolio-Subfolders-Projects-holder')
         holder.style.backgroundColor = 'blue'
         holder.style.color = 'white'
         holder.style.outline = '0.2vw solid black '
         holder.style.outlineStyle = 'dotted'
     }
     function go_blue_on_clickAboutMe(){
-        let holder = document.getElementById('RightPanel-Certifications-Subfolders-Dev-holder')
+        let holder = document.getElementById('RightPanel-Portfolio-Subfolders-AboutMe-holder')
         holder.style.backgroundColor = 'blue'
         holder.style.color = 'white'
         holder.style.outline = '0.2vw solid black '
@@ -46,17 +53,17 @@ const fileExplorer_RightPanel_Portfolio = () => {
 
     return(
         <div id='RightPanel-Portfolio-Subfolders-holder'>
-            <div id='RightPanel-Portfolio-Subfolders-Certifications-holder'>
+            <div id='RightPanel-Portfolio-Subfolders-Certifications-holder' onClick={go_blue_on_clickCyber}>
                 <div id='RightPanel-Portfolio-Subfolders-Certifications-icon'></div>
                 <div id='RightPanel-Portfolio-Subfolders-Certifications-text'>Certifications</div>
                 <div id='RightPanel-Portfolio-Subfolders-Certifications-type'>File Folder</div>
             </div>
-            <div id='RightPanel-Portfolio-Subfolders-Projects-holder'>
+            <div id='RightPanel-Portfolio-Subfolders-Projects-holder' onClick={go_blue_on_clickDev}>
                 <div id='RightPanel-Portfolio-Subfolders-Projects-icon'></div>
                 <div id='RightPanel-Portfolio-Subfolders-Projects-text'>Projects</div>
                 <div id='RightPanel-Portfolio-Subfolders-Projects-type'>File Folder</div>
             </div>
-            <div id='RightPanel-Portfolio-Subfolders-AboutMe-holder'>
+            <div id='RightPanel-Portfolio-Subfolders-AboutMe-holder' onClick={go_blue_on_clickAboutMe}>
                 <div id='RightPanel-Portfolio-Subfolders-AboutMe-icon'></div>
                 <div id='RightPanel-Portfolio-Subfolders-AboutMe-text'>About Me</div>
                 <div id='RightPanel-Portfolio-Subfolders-AboutMe-size'>10 KB</div>
